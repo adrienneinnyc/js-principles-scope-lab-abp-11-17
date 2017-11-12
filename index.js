f732fb21635ca463e61611d097913954b594998c
Adrienne Johnson <adriennetjohnson@gmail.com>
2017-11-12 20:11:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,10 @@
 // * `leastFavoriteCustomer` and `changeLeastFavoriteCustomer()`: Now declare a constant in global scope called `leastFavoriteCustomer`, be sure to assign it some initial value. Ok, now write a function called `changeLeastFavoriteCustomer()` that attempts to change that constant - notice what JavaScript does when you try to change the constant.
 // * `attemptTwoFavoriteCustomers()`: Now write another function that will break down. Try declaring a variable called `favoriteCustomer` and redeclaring that same variable such that an error is thrown. This helps illustrate how `let` only allows a given variable to be declared one time, but that variable can be reassigned to a different value. However, `const` does not allow reassignment, nor does it allow the same constant to be declared more than once.
 
-var customerName = "bob";
+let customerName = "bob";
 
 function upperCaseCustomerName(customerName) {
+  
   let upperCase = customerName.toUpperCase();
   return upperCase;
 }
