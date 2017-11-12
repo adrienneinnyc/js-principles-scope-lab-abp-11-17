1a269024b3b341d5adda7af5deffe0c6f97f5e97
Adrienne Johnson <adriennetjohnson@gmail.com>
2017-11-12 20:10:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@
 var customerName = "bob";
 
 function upperCaseCustomerName(customerName) {
-  return `${customerName.toUpperCase()}`;
+  let upperCase = customerName.toUpperCase();
+  return `${upperCase}`;
 }
-
